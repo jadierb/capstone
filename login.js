a4666760ca4a21f8a4340c136d54047c71643f0d
Jadier <jadierb@nycstudents.net>
2026-05-22 13:28:37 -0400

    put the style..cs oto idex.html

--- login.js
+++ login.js
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
     <head> 
-      <meta charset="UTf-8" />
-      <meta name="viewport" content="width=devide-width, initial-scale=1.0" />
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>log in - Wayfare</title>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
